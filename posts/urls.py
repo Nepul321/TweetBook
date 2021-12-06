c4d310ed7d7e76431dd0f84469e5e9c00f113550
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-05 22:20:59 -0500

    Add a post detail view

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from .views import (
-    post_list_view
+    post_list_view,
+    post_detail_view
 )
 
 from django.urls import path
 
 urlpatterns = [
     path('', post_list_view, name="post-list"),
+    path('<str:id>/', post_detail_view, name="post-detail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,9 @@ from rest_framework.decorators import (
 )
 from .models import Post
 from .serializers import PostSerializer
+# from rest_framework.permissions import (
+#     IsAuthenticated
+# )
 from rest_framework.response import Response
 
 @api_view(['GET'])
@@ -12,4 +15,14 @@ def post_list_view(request):
     context = {'request' : request}
     qs = Post.objects.all()
     serializer = PostSerializer(qs, many=True, context=context)
+    return Response(serializer.data, status=200)
+
+@api_view(['GET'])
+def post_detail_view(request, id):
+    context = {'request' : request}
+    qs = Post.objects.filter(id=id)
+    if not qs.exists():
+        return Response({'message' : 'Object not found'}, status=404)
+    obj = qs.first()
+    serializer = PostSerializer(obj, context=context)
     return Response(serializer.data, status=200)
