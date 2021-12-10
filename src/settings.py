0643c73dea5b0b8bd9a191f2fc96f38ab47a8e88
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-10 18:53:43 -0500

    Add password reset functionality

--- src/settings.py
+++ src/settings.py
@@ -158,6 +158,12 @@ REST_FRAMEWORK = {
    "DEFAULT_RENDERER_CLASSES" : DEFAULT_RENDERER_CLASSES
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
