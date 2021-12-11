548711797b4fe33b2a749e6ef6c7ffb3ea3664a8
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-10 20:52:07 -0500

    Create userkey db model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserKey(models.Model):
+    key = models.CharField(max_length=1000)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
