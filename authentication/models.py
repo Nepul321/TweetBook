5990615d10f49890ed8c3be36e8116fa4cf43a7b
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-12 19:10:57 -0500

    Update UserKey model

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 class UserKey(models.Model):
-    key = models.CharField(max_length=1000)
+    key = models.CharField(max_length=1000, default=uuid.uuid4())
     user = models.OneToOneField(User, on_delete=models.CASCADE)
