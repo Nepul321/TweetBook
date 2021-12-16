f8551ba6f7206391bbd29c5455462367ef94ab37
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-15 21:20:00 -0500

    Finish follow-unfollow functionality

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import redirect, render
-from profiles.models import Profile
 
 def HomeView(request):
     template = "pages/posts/home.html"
--- static/js/profiles/profile.js
+++ static/js/profiles/profile.js
@@ -1,11 +1,74 @@
 const container = document.querySelector(".container")
 
-function UpdateOnChange() {
-
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
 }
 
-function followUnFollow() {
+function UpdateOnChange(item) {
+    container.innerHTML = `
+    <div class="card mb-3">
+         <div class="row g-0">
+        <div class="col-md-4">
+        <img src="https://www.w3schools.com/howto/img_avatar.png" class="img-fluid rounded-start" alt="" />
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">${item.name} (@${item.username})</h5>
+            <p class="card-text">${item.bio}</p>
+            <p class="card-text">${item.location}</p>
+            <p class="card-text">Followers ${item.followers}, Following ${item.following}</p>
+            ${item.is_following === false ? `<button class="btn btn-primary" onclick="followUnFollow('follow')">Follow</button>` : `<button class="btn btn-primary"onclick="followUnFollow('unfollow')">Unfollow</button>`}
+            <p class="card-text my-3"><small class="text-muted">Joined on ${item.joined}</small></p>
+            <a href="" class="btn btn-outline-success">View posts &#8594;</a>
+        </div>
+        </div>
+    </div>
+    </div>
+    `
+}
 
+function followUnFollow(action) {
+    const endpoint = `/api/profiles/${container.id}/`
+    const data = {action : action}
+    const method = "POST"
+    const xhr = new XMLHttpRequest();
+    const csrftoken = getCookie("csrftoken");
+    xhr.open(method, endpoint)
+    xhr.setRequestHeader("Content-Type", "application/json");
+    if (csrftoken) {
+      xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const responseData = JSON.parse(xhr.response)
+        UpdateOnChange(responseData)
+      } else if(xhr.status === 404) {
+        alert("Profile does not exist")
+      } else if (xhr.status === 401 || xhr.status === 403) {
+        alert("You must login");
+        console.log(xhr.response)
+        // window.location.href = "/accounts/login/?next=/";
+      } else if (xhr.status === 500) {
+        alert("Please try again");
+      }    
+    }
+  
+    xhr.onerror = () => {
+      alert("An error occured. Please try again");
+    };
+  
+    xhr.send(JSON.stringify(data))
 }
 
 function InsertToRoot(item) {
@@ -21,14 +84,14 @@ function InsertToRoot(item) {
             <p class="card-text">${item.bio}</p>
             <p class="card-text">${item.location}</p>
             <p class="card-text">Followers ${item.followers}, Following ${item.following}</p>
-            ${item.is_following === false ? `<button class="btn btn-primary">Follow</button>` : `<button class="btn btn-primary">Unfollow</button>`}
+            ${item.is_following === false ? `<button class="btn btn-primary" onclick="followUnFollow('follow')">Follow</button>` : `<button class="btn btn-primary"onclick="followUnFollow('unfollow')">Unfollow</button>`}
             <p class="card-text my-3"><small class="text-muted">Joined on ${item.joined}</small></p>
+            <a href="" class="btn btn-outline-success">View posts &#8594;</a>
         </div>
         </div>
     </div>
     </div>
     `
-    console.log(item)
 }
 
 function loadProfile() {
