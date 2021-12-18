26245636d8872b6db32e0695b700aeaa594ed5eb
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-18 17:57:15 -0500

    Create comments DB model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+from posts.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    content = models.TextField(max_length=240)
+
+# class Reply(models.Model):
+#     comment = models.ForeignKey(Comment, blank=True, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+#     content = models.TextField(max_length=240)
+
+# class SubReply(models.Model):
+#     reply = models.ForeignKey(Reply, blank=True, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+#     content = models.TextField(max_length=240)
