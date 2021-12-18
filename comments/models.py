0916673d65f01a10e2608d05070ef4a79999f6b5
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-18 18:42:53 -0500

    Update comments/models.py

--- comments/models.py
+++ comments/models.py
@@ -5,14 +5,23 @@ from posts.models import Post
 class Comment(models.Model):
     post = models.ForeignKey(Post, blank=True, on_delete=models.CASCADE)
     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name="comment_likes")
     content = models.TextField(max_length=240)
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
 
 class Reply(models.Model):
     comment = models.ForeignKey(Comment, blank=True, on_delete=models.CASCADE)
     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name="comment_likes")
     content = models.TextField(max_length=240)
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
 
 class SubReply(models.Model):
     reply = models.ForeignKey(Reply, blank=True, on_delete=models.CASCADE)
     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name="comment_likes")
     content = models.TextField(max_length=240)
+    date = models.DateField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
