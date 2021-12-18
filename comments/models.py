f3c2865723e010c9752b168a9505295464b18fbb
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-18 17:58:12 -0500

    Create replies  DB model

--- comments/models.py
+++ comments/models.py
@@ -7,10 +7,10 @@ class Comment(models.Model):
     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
     content = models.TextField(max_length=240)
 
-# class Reply(models.Model):
-#     comment = models.ForeignKey(Comment, blank=True, on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
-#     content = models.TextField(max_length=240)
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    content = models.TextField(max_length=240)
 
 # class SubReply(models.Model):
 #     reply = models.ForeignKey(Reply, blank=True, on_delete=models.CASCADE)
