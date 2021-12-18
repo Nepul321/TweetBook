8ae67e379c911a93b24d697a46ce0e4ceda06555
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-18 18:38:28 -0500

    Create comment details view

--- comments/urls.py
+++ comments/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
+    comment_details,
     comment_list
 )
 
 urlpatterns = [
     path('', comment_list, name="comments"),
+    path("<int:id>/", comment_details, name="comment-details"),
 ]
--- comments/views.py
+++ comments/views.py
@@ -15,3 +15,14 @@ def comment_list(request):
     qs = Comment.objects.all()
     serializer = CommentSerializer(qs, many=True, context=context)
     return Response(serializer.data, status=200)    
+
+@api_view(['GET'])
+def comment_details(request, id):
+    context = {'request' : request}
+    qs = Comment.objects.filter(id=id)
+    if not qs:
+        return Response({"message" : "Not found"})
+    obj = qs.first()
+    serializer = CommentSerializer(obj, context=context)
+
+    return Response(serializer.data, status=200)
