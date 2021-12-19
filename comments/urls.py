971e886754fcc988c6f90ce50eaff3d64698e02e
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-18 19:33:19 -0500

    Rename comment_details view

--- comments/urls.py
+++ comments/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import (
     comment_create,
-    comment_details,
+    comment_details_delete,
     comment_list,
     post_comments_list
 )
 
 urlpatterns = [
     path('', comment_list, name="comments"),
-    path("<int:id>/", comment_details, name="comment-details"),
+    path("<int:id>/", comment_details_delete, name="comment-details"),
     path('create/', comment_create, name="comment-create"),
     path('post/<int:id>/', post_comments_list, name="post-comments-list"),
 ]
